2a2925a75de3d8095f5d28a56046c567890e5489
d1rkdaniil-code <d1rkdaniil@gmail.com>
2026-06-18 02:41:12 +0300

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from app import create_app
+from app.extensions import db
+
+@pytest.fixture(scope='module')
+def app():
+    app = create_app('testing')
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
